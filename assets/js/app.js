7bdb9cc9ecd599d08cd9d6552318976214be946c
JoelEkpenyong <joelekpenyong@gmail.com>
2020-07-26 00:37:01 +0100

    updated

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,7 @@ const text = intro.querySelector('h1');
 const controller = new ScrollMagic.Controller();
 // scenes
 let scene = new ScrollMagic.Scene({
-      duration: 48000,
+      duration: 17000,
       triggerElement: intro,
       triggerHook: 0
 }).setPin(intro)
@@ -14,22 +14,22 @@ let scene = new ScrollMagic.Scene({
 
 
 // text animation
-const textAnim1 = gsap.to(text, {opacity: 0, duration:2})
+const textAnim1 = gsap.to(text, {opacity: 0, duration:.5})
 
 let scene2 = new ScrollMagic.Scene({
-    duration: 2000,
+    duration: 500,
     triggerElement: intro,
     triggerHook: 0
 }).setTween(textAnim1)
   .addTo(controller);
 
 // video animation
-let accelamount = 0.01;
+let accelamount = 0.1;
 let scrollpos = 0;
 let delay = 0;
 
 scene.on('update', function(e){
-    scrollpos = e.scrollPos / 600;
+    scrollpos = e.scrollPos / 1000;
 });
 setInterval(function(){
     delay += (scrollpos - delay) * accelamount 
